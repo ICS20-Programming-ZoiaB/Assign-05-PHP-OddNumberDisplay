3dcae59a90df34edfb71b388fa60d64b262426a9
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-18 21:22:13 +0000

    Changed comments

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-ca">
+  <head>
+    
+    <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="Odd Number Display, PHP">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="ZoiaB">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/android-chrome-512x512.png">
+    <link rel="manifest" href="./fav_index/android-chrome-512x512.png">
+    
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-deep_orange.min.css" />
+   
+    <!-- Title -->
+    <title>Odd Number Display, PHP</title>
+  </head>
+  <body>
+    <?php echo "<h1>Odd Number Display, PHP</h1>"; ?>
+  
+    <!-- Header and text -->
+      <?php
+        echo "<p>Input a positive integer and this webpage will display all the odd numbers up to your number, from the maximum number.</p>";
+        echo "<h3>Your Input:</h3>"; ?>
+    
+    <!-- Form for user input -->
+    <form action="./results.php" method="post" target="results">
+      <label for="min number">Enter the starting number:</label>
+      <input type="number" step="1" name="min-number" placeholder="Minimum"><br><br>
+      <label for="max number">Enter your stopping number:</label>
+      <input type="number" step="1" name="max-number" placeholder="Maximum"><br><br>
+      <input type="submit" value="Submit Numbers" id="submit-button" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+    </form>
+
+    <!-- Iframe -->
+    <br>
+    <iframe id="results" name="results"></iframe>
+
+    <!-- Image -->
+    <br>
+    <img src="./images/odd-number-chart.jpg" alt="Odd Numbers" length="400" width="400">
+  </body>
+</html>
---
+++ results.php
@@ -0,0 +1,43 @@
+<?php
+  //initializing the numbers and message variable to an empty string
+  $numbers = "";
+  $message = "";
+
+  //getting the user values for the minimum and maximum numbers
+  $minNumber = intval($_POST['min-number']);
+  $maxNumber = intval($_POST['max-number']);
+
+  //initializing the counter variable to the minNumber
+  $counter = $minNumber;
+
+  //If statement checks to verify that user input is valid
+  if ((is_nan($minNumber) || is_nan($maxNumber)) || empty($_POST['min-number']) || empty($_POST['max-number'])) {
+  $message = "Please enter valid numbers.";
+}
+
+  // if statement checks if either number is negative
+  else if ($minNumber < 0 || $maxNumber < 0) {
+    $message = "Enter positive integers for both the minimum and maximum.";
+  }
+
+  // if statement checks if minNumber > maxNumber (there is an error)
+  else if ($minNumber > $maxNumber) {
+    $message = "Ensure that the minimum is less than or equal to the maximum.";
+  }
+
+  //else, complete while loop to create list of even numbers between min and max
+  else {
+    //while loop that specifies the range
+    while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
+      //if statement checks for even numbers
+      if (($counter <= $maxNumber) && ($counter % 2 == 1)) {
+        $numbers = $numbers . $counter . "<br>";
+      }
+      $counter = $counter + 1;
+      $message = "Here is a list of all the odd numbers between the minimum and maximum that you entered:<br><br>$numbers";
+    }
+  }
+
+  //displaying the results (the range of even numbers) back to the user
+  echo "$message";
+?>
