983e8caf3e174ff79949bc5e4576b1a735bb6de7
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-26 14:43:37 +0000

    updated comments

--- results.php
+++ results.php
@@ -7,47 +7,47 @@ $message = "";
 $minNumber = $_POST['min-number'];
 $maxNumber = $_POST['max-number'];
 
-// If statement checks to verify that user input is valid using is_numeric (method from: https://www.w3schools.com/php/func_va r_is_numeric.asp)
+//if: if user does not enter input or if invalid input is entered, tell user to enter valid numbers. Using is_numeric (method from: https://www.w3schools.com/php/func_va r_is_numeric.asp)
 if (!is_numeric($minNumber) || !is_numeric($maxNumber)) {
   $message = "Please enter valid numbers.";
 }
   
-// If minNumber > maxNumber (there is an error)
+//else if: otherwise, if minimum (starting) number is greater than maximum (stopping) number, tell user to ensure that the starting number is less than or equal to the stopping number
 else if ($minNumber > $maxNumber) {
   $message = "Ensure that the starting number is less than or equal to the stopping number.";
 }
   
-// Else, generate the list of odd numbers based on user input. 
+//else, complete a loop to display list of odd numbers
 else {
   
-  //Gets result of which radio button to determine whether list of odd numbers should be reversed or not using "isset" (method taken from: https://www.simplilearn.com/tutorials/php-tutorial/isset-inphp#:~:text=The%20isset()%20function%20dete rmines,%3B%20else%2C%20it%20returns%20FALSE.)
+  //Determines which radio button to determine whether list of odd numbers should be reversed or not using "isset" (method taken from: https://www.simplilearn.com/tutorials/php-tutorial/isset-inphp#:~:text=The%20isset()%20function%20dete rmines,%3B%20else%2C%20it%20returns%20FALSE.)
   $reversed = isset($_POST['order']) && $_POST['order'] === 'reversed';
-  
   if ($reversed) {
     $counter = $maxNumber;
-    // Use while loop to spefify range to generate the reversed list of odd numbers
+    //Do while loop to display list of odd numbers
     do {
          // if statement checks for odd numbers, provided that the condition in the while statement is true. It uses an operator that determines if there is a remainder when the number is divided by 2 (method taken from https://www.tutorialspoint.com/How-to-determine-if-a-number-is-odd-or-even-in-JavaScript)
       if ($counter % 2 !== 0) {
         $numbers .= $counter . "<br>";
       }
       $counter = $counter - 1;
-      //while counter is bigger than or equal to minimum
     } while ($counter >= $minNumber);
     $message = "Here are all the odd numbers between your selected numbers:<br><br>" . $numbers;
   }
-  
+
+   // else, complete while loop to create list of odd numbers between minimum (starting) number and maximum (stopping) number in regular order
   else {
-    // Use while loop to generate the normal list of odd numbers in regular order
     $counter = $minNumber;
     while ($counter <= $maxNumber) {
+      // if statement checks for odd numbers, provided that the condition in the while statement is true. It uses an operator that determines if there is a remainder when the number is divided by 2 (method taken from https://www.tutorialspoint.com/How-to-determine-if-a-number-is-odd-or-even-in-JavaScript)
       if ($counter % 2 !== 0) {
         $numbers .= $counter . "<br>";
       }
       $counter++;
     }
   }
-  
+
+  //Set message variable equal to the list of odd numbers
   $message = "Here are all the odd numbers between your selected numbers:<br><br>" . $numbers;
 }
 
