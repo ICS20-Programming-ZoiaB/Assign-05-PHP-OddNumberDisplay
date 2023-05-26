b4f7dbc3087bf85ff1617df89263e4cd3eceae7c
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-26 01:54:01 +0000

    Changed the way radio buttons were linked to the if statement that determined if it should be reversed

--- index.php
+++ index.php
@@ -1,66 +1,61 @@
 <!DOCTYPE html>
 <html lang="en-ca">
-  <head>
-    
-    <!-- Metadata -->
-    <meta charset="utf-8">
-    <meta name="description" content="Odd Number Display, PHP">
-    <meta name="keywords" content="immaculata, ics2o">
-    <meta name="author" content="ZoiaB">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/android-chrome-512x512.png">
-    <link rel="manifest" href="./fav_index/android-chrome-512x512.png">
-    
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-deep_orange.min.css" />
-   
-    <!-- Title -->
-    <title>Odd Number Display, PHP</title>
-  </head>
-  <body>
-    <?php echo "<h1>Odd Number Display, PHP</h1>"; ?>
+<head>
+  <!-- Metadata -->
+  <meta charset="utf-8">
+  <meta name="description" content="Odd Number Display, PHP">
+  <meta name="keywords" content="immaculata, ics2o">
+  <meta name="author" content="ZoiaB">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
-    <!-- Header and text -->
-    <?php
-      echo "<p>Input a positive starting number and then a positive stopping number, and this webpage will give you all the odd numbers in between!</p>";
-      echo "<h3>Your Input:</h3>"; ?>
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/android-chrome-512x512.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/android-chrome-512x512.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/android-chrome-512x512.png">
+  <link rel="manifest" href="./fav_index/android-chrome-512x512.png">
+  
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-deep_orange.min.css" />
+
+  <!-- Title -->
+  <title>Odd Number Display, PHP</title>
+</head>
+<body>
+  <h1>Odd Number Display, PHP</h1>
+
+  <!-- Header and text -->
+  <p>Input a positive starting number and then a positive stopping number, and this webpage will give you all the odd numbers in between!</p>
+  <h3>Your Input:</h3>
+
+  <!-- Form for user input -->
+  <form action="./results.php" method="post" target="results">
+    <label for="min-number">Number you would like the list to start at:</label>
+    <input type="number" step="1" name="min-number" placeholder="Minimum"><br><br>
+    <label for="max-number">Enter your stopping number:</label>
+    <input type="number" step="1" name="max-number" placeholder="Maximum"><br><br>
     
-    <!-- Form for user input -->
-    <form action="./results.php" method="post" target="results">
-      <label for="min number">Number you would like the list to start at:
-</label>
-      <input type="number" step="1" name="min-number" placeholder="Minimum"><br><br>
-      <label for="max number">Enter your stopping number:</label>
-      <input type="number" step="1" name="max-number" placeholder="Maximum"><br><br>
-      <input type="submit" value="Submit Numbers" id="submit-button" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
-    </form>
-    
-    <!-- Radio buttons for option to display list in reverse -->
-    <br>
-    <?php echo "<p>Do you want the list to be in reverse order?</p>"; ?>
-    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="reversed">
-        <input type="radio" id="reversed" class="mdl-radio__button" name="reversed" value="reversed">
-        <span class="mdl-radio__label">Yes</span>
-      </label>
+    <p>Do you want the list to be in reverse order?</p>
+    <label>
+      <input type="radio" name="order" value="reversed">
+      <span>Yes</span>
+    </label>
+    <label>
+      <input type="radio" name="order" value="not-reversed">
+      <span>No</span>
+    </label><br><br>
 
-      <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="not-reversed">
-        <input type="radio" id="not-reversed" class="mdl-radio__button" name="not-reversed" value="2">
-        <span class="mdl-radio__label">No</span>
-      </label>
+    <input type="submit" value="Submit Numbers" id="submit-button" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+  </form>
 
-    <!-- Iframe -->
-    <br>
-    <iframe id="results" name="results"></iframe>
+  <!-- Iframe -->
+  <br>
+  <iframe id="results" name="results"></iframe>
 
-    <!-- Image -->
-    <center>
+  <!-- Image -->
+  <center>
     <br>
     <img src="./images/odd-number-chart.jpg" alt="Odd Numbers" length="400" width="400">
-    </center>
-  </body>
+  </center>
+</body>
 </html>
