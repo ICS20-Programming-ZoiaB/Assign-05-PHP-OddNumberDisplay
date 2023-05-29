33162e00bd983c119152ee8a91cc299f77c488d9
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-29 15:37:32 +0000

    Fixed css comments

--- results.php
+++ results.php
@@ -20,11 +20,11 @@ else if ($minNumber > $maxNumber) {
 //else, complete a loop to display list of odd numbers
 else {
   
-  //Determines which radio button to determine whether list of odd numbers should be reversed or not using "isset" (method taken from: https://www.simplilearn.com/tutorials/php-tutorial/isset-inphp#:~:text=The%20isset()%20function%20dete rmines,%3B%20else%2C%20it%20returns%20FALSE.)
+  //Determines which radio button is checked to determine whether list of odd numbers should be reversed or not using "isset" (method taken from: https://www.simplilearn.com/tutorials/php-tutorial/isset-inphp#:~:text=The%20isset()%20function%20dete rmines,%3B%20else%2C%20it%20returns%20FALSE.)
   $reversed = isset($_POST['order']) && $_POST['order'] === 'reversed';
   if ($reversed) {
     $counter = $maxNumber;
-    //Do while loop to display list of odd numbers
+    //Do while loop to display list of odd numbers in reverse
     do {
          // if statement checks for odd numbers, provided that the condition in the while statement is true. It uses an operator that determines if there is a remainder when the number is divided by 2 (method taken from https://www.tutorialspoint.com/How-to-determine-if-a-number-is-odd-or-even-in-JavaScript)
       if ($counter % 2 !== 0) {
